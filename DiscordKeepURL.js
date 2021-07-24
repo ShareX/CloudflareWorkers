d00d9609f4b19fe2628d3876bac5861befa2dc34
Jaex <Jaex@users.noreply.github.com>
2021-07-24 23:43:35 +0300

    Added DiscordKeepURL.js

--- DiscordKeepURL.js
+++ DiscordKeepURL.js
@@ -5,26 +5,11 @@ async function handleRequest(request) {
     if (response.status == 200) {
       const contentType = response.headers.get("content-type");
       if (contentType && contentType.startsWith("image/")) {
-        const url = new URL(request.url);
-        const filename = url.pathname.substr(url.pathname.lastIndexOf("/") + 1);
-        const title = "ShareX";
-        const description = filename;
-        const color = "#00aff4";
         return new Response(`<html>
 <head>
-  <title>${title}</title>
-  <meta charset="utf-8">
-  <meta name="robots" content="noindex">
-  <meta name="theme-color" content="${color}">
-  <meta name="description" content="${description}">
-  <meta property="og:title" content="${title}">
-  <meta property="og:description" content="${description}">
-  <meta property="og:image" content="${url.toString()}">
   <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:image" content="${request.url}">
 </head>
-<body>
-  <img src="${url.toString()}">
-</body>
 </html>`, {
           headers: {
             "content-type": "text/html"
