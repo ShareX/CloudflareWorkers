f81c73250bf8d5679dfbd34c2925a7f383c339aa
Rhys <72263189+givinghawk@users.noreply.github.com>
2021-12-29 22:20:45 +0000

    Update embed on discord
    Added const:
    const username = "Your Username";
    
    Added twitter card image incase of og:image not working:
    <meta name="twitter:image" content="${url.toString()}">
    
    Added og:site_name:
    <meta property="og:site_name" content="${username}">

--- DiscordEmbed.js
+++ DiscordEmbed.js
@@ -10,16 +10,19 @@ async function handleRequest(request) {
         const title = "ShareX";
         const description = filename;
         const color = "#00aff4";
+        const username = "Your Username";
         return new Response(`<html>
 <head>
   <title>${title}</title>
   <meta charset="utf-8">
+  <meta property="og:site_name" content="${username}">
   <meta name="robots" content="noindex">
   <meta name="theme-color" content="${color}">
   <meta property="og:title" content="${title}">
   <meta property="og:description" content="${description}">
   <meta property="og:image" content="${url.toString()}">
   <meta property="twitter:card" content="summary_large_image">
+  <meta name="twitter:image" content="${url.toString()}">
 </head>
 <body>
   <img src="${url.toString()}">
